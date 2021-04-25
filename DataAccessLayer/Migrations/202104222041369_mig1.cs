7e674b3f6259d6590fa8b85998e0508f3fc63523
Ramazan İPERLİK <ramazaniperlik@gmail.com>
2021-04-26 00:48:01 +0300

    MvcProje v1

---
+++ DataAccessLayer/Migrations/202104222041369_mig1.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Headings", "AuthorID", c => c.Int());
+            CreateIndex("dbo.Headings", "AuthorID");
+            AddForeignKey("dbo.Headings", "AuthorID", "dbo.Authors", "AuthorID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Headings", "AuthorID", "dbo.Authors");
+            DropIndex("dbo.Headings", new[] { "AuthorID" });
+            DropColumn("dbo.Headings", "AuthorID");
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        [Key]
+        public int HeadingID { get; set; }
+
+
+        [StringLength(50)]
+        public string HeadingName { get; set; }
+        public DateTime Date { get; set; }
+
+
+        public int CategoryID { get; set; }
+        public virtual  Category Category { get; set; }
+
+        public int? AuthorID { get; set; }
+        public virtual Author Author { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
+    }
+}
